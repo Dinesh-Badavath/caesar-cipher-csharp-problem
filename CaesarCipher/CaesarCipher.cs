889d25cc0e05668974cd09d5eb0b661befd47673
dinesh.badavath <dinesh.badavath@cgi.com>
2019-08-29 16:40:02 +0530

    Solved

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,10 +7,45 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+             char[] Arr = text.ToCharArray();
+        for (int i = 0; i < Arr.Length; i++)
+        {
+           if(!char.IsLetter(Arr[i])){
+                Arr[i]=Arr[i];
+}            else{                        
+            char letter = Arr[i];
+            
+            letter = (char)(letter + shiftKey);
+                if(!char.IsUpper(Arr[i])){
+         
+            if (letter > 'z')
+            {
+                letter = (char)(letter - 26);
+            }
+            else if (letter < 'a')
+            {
+                letter = (char)(letter + 26);
+            }}
+     if(char.IsUpper(Arr[i])){
+                    
+            if (letter > 'Z')
+            {
+                letter = (char)(letter - 26);
+            }
+            else if (letter < 'A')
+            {
+                letter = (char)(letter + 26);
+            }
+}
+     
+            Arr[i] = letter;
+        }  } 
+        return new string(Arr);
         }
         public static void Main(string[] args){
-            
+            string text=Console.ReadLine();
+            int shiftKey=Convert.ToInt32(Console.ReadLine());
+            string Cipher = Rotate(text,shiftKey);
         }
     }
 }
